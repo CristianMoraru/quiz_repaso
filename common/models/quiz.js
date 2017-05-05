29fe8142cd4b8c2b806245e09b7d2fc6ca6f9191
angel2433282 <billyjohe.com>
2017-05-05 11:35:11 +0200

    mmy work

--- common/models/quiz.js
+++ common/models/quiz.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = function(Quiz) {
+/*
+	Quiz.beforeRemote( asocia, function( ctx, next) {
+	    //...
+	    var token = app.models.accessToken;
+	    if (token) {
 
+	    }
+	    next();
+	});
+*/
 };
